fe5c8715052555c4f8fb705f01e7625a3293251d
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-09-06 18:50:04 -0500

    incomplete code for the 4th problem

---
+++ Meeting 1/Problem D/code.js
@@ -0,0 +1,12 @@
+const readline = require('readline-sync');
+
+let input = readline.question("give numbers plz");
+var fields = input.split(' ');
+// https://stackoverflow.com/a/96452
+
+var stoneBridges = fields[0];
+var knights = fields[1];
+var groupSize = fields[2];
+var days = ;
+
+console.log();
