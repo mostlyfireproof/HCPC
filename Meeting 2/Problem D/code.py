2faaffc13d425e136ede48966880896bd1c74d6f
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-09-13 19:16:31 -0500

    small change, still doesn't pass all tests

--- Meeting 2/Problem D/code.py
+++ Meeting 2/Problem D/code.py
@@ -1,9 +1,8 @@
 # https://open.kattis.com/problems/licensetolaunch
-import math
 
 line1 = input()
 line2 = input()
 
 data = line2.split()
 
-print(data.index(min(data)))
+print(int(data.index(min(data))))
