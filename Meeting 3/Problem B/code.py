f7d576165fc41c846e01245083492f829e5bd182
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-09-20 18:55:10 -0500

    progress

--- Meeting 3/Problem B/code.py
+++ Meeting 3/Problem B/code.py
@@ -5,4 +5,15 @@ data = input().split()
 lights = int(data[0])
 length = int(data[1])
 
-numLights = (len(data) - 2) / 3
+time = 0
+lightsPassed = 0
+
+for x in range(0, length)
+    if x = data[lightsPassed]
+        cycle = data[1 + lightsPassed] + data[2 + lightsPassed]
+        # code goes here
+        lightsPassed += 3
+    time += 1
+
+
+print(time)
