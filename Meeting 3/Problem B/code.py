605daf49c07db806bfcfb9ba02666206ed6d7a42
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-09-20 19:07:31 -0500

    it runs (not working fully though)

--- Meeting 3/Problem B/code.py
+++ Meeting 3/Problem B/code.py
@@ -2,16 +2,21 @@
 
 data = input().split()
 
-lights = int(data[0])
+lines = int(data[0])
 length = int(data[1])
+
+lights = []
+for i in range(lines):
+    lights.append(input().split())
 
 time = 0
 lightsPassed = 0
 
-for x in range(0, length)
-    if x = data[lightsPassed]
-        cycle = data[1 + lightsPassed] + data[2 + lightsPassed]
-        # code goes here
+for x in range(0, length):
+    if x == lights[lightsPassed]:
+        cycle = lights[1 + lightsPassed] + lights[2 + lightsPassed]
+        while (time % cycle) >= lights[1 + lightsPassed]:
+            time += 1
         lightsPassed += 3
     time += 1
 
