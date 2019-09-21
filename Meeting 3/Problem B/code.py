e81041415c645db5326ce7348453e4242576fadf
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2019-09-20 19:43:05 -0500

    debug time

--- Meeting 3/Problem B/code.py
+++ Meeting 3/Problem B/code.py
@@ -5,20 +5,30 @@ data = input().split()
 lines = int(data[0])
 length = int(data[1])
 
-lights = []
+lightsRaw = []
 for i in range(lines):
-    lights.append(input().split())
+    lightsRaw.append(input().split())
+
+
+lights = sum(lightsRaw, [])
+for j in range(0, len(lights)):
+    lights[j] = int(lights[j])
 
 time = 0
 lightsPassed = 0
 
-for x in range(0, length):
+for x in range(0, length-1):
+    print("heck")
     if x == lights[lightsPassed]:
+        print("edgub")
         cycle = lights[1 + lightsPassed] + lights[2 + lightsPassed]
-        while (time % cycle) >= lights[1 + lightsPassed]:
+        while (time % cycle) >= lights[2 + lightsPassed]:
             time += 1
         lightsPassed += 3
     time += 1
 
-
+print(lights)
+print(lines)
+print(length)
+print(lightsPassed)
 print(time)
