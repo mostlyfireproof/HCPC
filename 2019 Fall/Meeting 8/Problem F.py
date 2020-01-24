9d23241164f0d3cf5e48485322614db6ecfe1620
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2020-01-24 18:07:04 -0500

    moved things around for the new semester

--- 2019 Fall/Meeting 8/Problem F.py
+++ 2019 Fall/Meeting 8/Problem F.py
@@ -5,12 +5,12 @@ length = int(input())
 priceList = input().split()
 
 priceList = [int(i) for i in priceList]
-priceList.sort()
+priceList.sort(reverse=True)
 
 thingsToBePaidFor = 0
 
 for i in range(0, length):
-    if 0 == (i % 3):
+    if 2 == (i % 3):
         thingsToBePaidFor += priceList[i]
     else:
         thingsToBePaidFor
