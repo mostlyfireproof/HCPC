724358efa9692cf3284f60ec21a092599cfc35f1
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2020-01-24 19:30:28 -0500

    g works on my computer, but breaks theirs

---
+++ 2020 Spring/1/G.py
@@ -0,0 +1,16 @@
+# https://open.kattis.com/problems/egypt
+
+lines = []
+while True:
+    line = input()
+    if line:
+        lines.append(line)
+    else:
+        break
+
+for j in range(0, (len(lines) - 1)):
+    data = lines[j].split()
+    if ((int(data[0]) * int(data[0]) + int(data[1]) * int(data[1])) == int(data[2]) * int(data[2])):
+        print("right")
+    else:
+        print("wrong")
